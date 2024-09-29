2c15b3015b3822a934942544a9884b05669bf3dc
Ilya <132289743+IlyaKA54@users.noreply.github.com>
2024-09-29 18:43:31 +0300

    init

---
+++ app/src/main/java/com/example/fitnessclub/data/Membership.kt
@@ -0,0 +1,9 @@
+package com.example.fitnessclub.data
+
+data class Membership(
+    val name: String = "",
+    val description: String = "",
+    val price: String = "",
+    val category: String = "",
+    val imageUrl: String = ""
+)
